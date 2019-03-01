8ba5d8e9c66aab6a2d07301dda8db254f0dedc5c
LNEUENSCH001 <lneuensch001@vplincsci021.ad.csbsju.edu>
2019-02-28 21:24:41 -0600

    Jackson Levi Commit

---
+++ src/AdminFunctionalities/Admin.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package AdminFunctionalities;
+
+/**
+ * @author lneuensch001
+ *
+ */
+public class Admin {
+
+	/**
+	 * 
+	 */
+	public Admin() {
+		Account admin = new Admin();
+	}
+	 /**
+	  * Sets the type for an account
+	  * 
+	  * @param type
+	  */
+	public void setType(String type) {
+			//TODO 
+	}
+	/**
+	 * Sets the status for an account
+	 * 
+	 * @param status
+	 */
+	public void setStatus(String status) {
+			//TODO
+	}
+}
---
+++ src/other/AccountController.java
@@ -0,0 +1,102 @@
+/**
+ * 
+ */
+package other;
+
+/**
+ * @author lneuensch001
+ *
+ */
+public class AccountController {
+
+	/**
+	 * 
+	 */
+	public AccountController() {
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * 
+	 * @param username
+	 * @param password
+	 */
+	public void login(String username, String password) {
+		//TODO
+	}
+
+	/**
+	 * 
+	 */
+	public void logOut() {
+		//TODO
+	}
+	
+	/**
+	 * 
+	 * @param username
+	 */
+	public void isUserReal(String username) {
+		//TODO
+	}
+	
+	/**
+	 * 
+	 * @param username
+	 * @return
+	 */
+	public String getPassword(String username) {
+		//TODO
+		return "";
+	}
+	
+	/**
+	 * 
+	 * @param username
+	 */
+	public void viewProfile(String username) {
+		//TODO
+	}
+	
+	/**
+	 * 
+	 * @param username
+	 */
+	public void editProfile(String username) {
+		//TODO
+	}
+	
+	/**
+	 * 
+	 * @param O
+	 * @param n1
+	 * @param n2
+	 */
+	public void resetPassword(String O, String n1, String n2) {
+		//TODO
+	}
+	
+	/**
+	 * 
+	 * @param email
+	 * @param firstName
+	 * @param lastName
+	 */
+	public void recoverPassword(String email, String firstName, String lastName) {
+		//TODO
+	}
+	
+	/**
+	 * 
+	 * @param jpeg
+	 */
+	public void addProfilePicture(String jpeg) {
+		//TODO
+	}
+	/**
+	 * 
+	 * @param password
+	 */
+	public void passwordRequirements(String password){
+		//TODO
+	}
+}
