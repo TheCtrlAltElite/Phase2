239692fb3d6cb261a049ae54f942e2a2aed95b95
LNEUENSCH001 <lneuensch001@vplincsci089.ad.csbsju.edu>
2019-03-18 18:21:55 -0500

    tiny changes

--- src/UserFunctionalities/User.java
+++ src/UserFunctionalities/User.java
@@ -29,7 +29,7 @@ public class User extends Account{
 	/**
 	 * Searches schools based on the criteria the user gave.
 	 */
-	public void searchSchools(String schoolName, String schoolState, String schoolLocation, String schoolControl, String numberStudents, int percentFemale, int verbalSAT, int mathSAT, int schoolExpenses, int percentFinancialAid, int numApplicants, int percentAdmitted, int percentEnrolled, int academicScale, int socialScale, int qualityScale) {
+	public void searchSchools(String schoolName, String schoolState, String schoolLocation, String schoolControl, String numberStudents, String percentFemale, String verbalSAT, String mathSAT, String schoolExpenses, String percentFinancialAid, String numApplicants, String percentAdmitted, String percentEnrolled, String academicScale, String socialScale, String qualityScale) {
 		userInteract.searchSchools(schoolName, schoolState, schoolLocation, schoolControl, numberStudents, percentFemale, verbalSAT, mathSAT, schoolExpenses, percentFinancialAid, numApplicants, percentAdmitted, percentEnrolled, academicScale,  socialScale, qualityScale);
 	}
 	
