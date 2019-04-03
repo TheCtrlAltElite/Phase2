517b55925a5b4316d1dca08e95f9813675fdd558
JMUEHLENB001 <jmuehlenb001@vplincsci019.ad.csbsju.edu>
2019-04-03 15:48:32 -0500

    UniversityController tests

--- tests/UniversityFunctionalitiesTests/UniversityControllerTest.java
+++ tests/UniversityFunctionalitiesTests/UniversityControllerTest.java
@@ -2,6 +2,8 @@ package UniversityFunctionalitiesTests;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.After;
 import org.junit.Test;
@@ -28,7 +30,33 @@ public class UniversityControllerTest {
 		assertFalse("Universtiy is not in the Database", result);
 	}
 	
-
+	@Test
+	public void editUniversityChangesFields() {
+		uc.editUniversity("CSBSJU", "MN", "URBAN", "STATE", 155, 155, 155, 155, 155, 155, 155, 155, 155, 5, 5, 5);
+		List<String> details = uc.getDetailsUni("CSBSJU");
+		assertTrue(details.get(0).equals("CSBSJU"));
+		assertTrue(details.get(1).equals("MN"));
+		assertTrue(details.get(2).equals("URBAN"));
+		assertTrue(details.get(3).equals("STATE"));
+		assertTrue(details.get(4).equals("155"));
+		assertTrue(details.get(5).equals("155"));
+		assertTrue(details.get(6).equals("155"));
+		assertTrue(details.get(7).equals("155"));
+		assertTrue(details.get(8).equals("155"));
+		assertTrue(details.get(9).equals("155"));
+		assertTrue(details.get(10).equals("155"));
+		assertTrue(details.get(11).equals("155"));
+		assertTrue(details.get(12).equals("155"));
+		assertTrue(details.get(13).equals("5"));
+		assertTrue(details.get(14).equals("5"));
+		assertTrue(details.get(15).equals("5"));	
+	}
+	
+	@Test
+	public void editUniversityFailsForInvalidUniversity() {
+		boolean result = uc.editUniversity("IDKWHATSGOINGON", "MN", "URBAN", "STATE", 155, 155, 155, 155, 155, 155, 155, 155, 155, 5, 5, 5);
+		assertFalse("Edit University value is FALSE", result);
+	}
 	
 	
 
