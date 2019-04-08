c1317052637e9e6b2be6a3660e62880cea17569e
OMATIAS001 <omatias001@vplincsci008.ad.csbsju.edu>
2019-04-08 16:03:40 -0500

    osbaldo tring to pull

--- tests/AdminFunctionalitiesTests/AdminFunctionalityControllerTest.java
+++ tests/AdminFunctionalitiesTests/AdminFunctionalityControllerTest.java
@@ -14,22 +14,38 @@ import UserFunctionalities.User;
 
 public class AdminFunctionalityControllerTest {
 
-	private AdminFunctionalityController adc;
+	private AdminFunctionalityController afc;
 	
 	@Before
 	public void setUp() {
-		this.adc = new AdminFunctionalityController();
+		this.afc = new AdminFunctionalityController();
+	}
+	
+	
+	@Test
+	public void addToSavedSchoolsListSuccess() {
+		boolean result = afc.addToSavedSchoolsList3("juser", "BARD");
+		assertTrue("Universtiy was successfully added.", result);
 	}
 	
 	@Test
+	public void addToSavedSchoolsListInvalidSchool() {
+		boolean result = afc.addToSavedSchoolsList3("tux69@csbsju.edu","NOTASCHOOL");
+		assertFalse("Universtiy is not in the Database", result);
+	}
+	
+	
+	
+	
+	@Test
 	public void viewUniversitiesTest() {
-		List<University> listUnis = adc.viewUniversities();
+		List<University> listUnis = afc.viewUniversities();
 		assertTrue(listUnis.size() == 181);
 	}
 	
 	@Test
 	public void viewUsersTest() {
-		List<Account> listUsers = adc.viewUsers();
+		List<Account> listUsers = afc.viewUsers();
 		assertTrue(listUsers.size() == 7);
 	}
 	
