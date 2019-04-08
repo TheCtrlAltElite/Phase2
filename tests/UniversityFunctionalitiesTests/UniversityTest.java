5e6a296b35d8119d3f16ae5c653567457b0975be
RCLINTSMA001 <rclintsma001@vplincsci059.ad.csbsju.edu>
2019-04-08 10:06:16 -0500

    UniveristyTest 86%

--- tests/UniversityFunctionalitiesTests/UniversityTest.java
+++ tests/UniversityFunctionalitiesTests/UniversityTest.java
@@ -158,6 +158,68 @@ public class UniversityTest {
 	
 	@Test
 	public void setSchoolStateTest() {
-		
+		uni.setSchoolState("OHIO");
+		assertTrue("The University's State was set to OHIO", uni.getSchoolState().equals("OHIO"));
+	}
+	
+	@Test
+	public void setSchoolLocationTest() {
+		uni.setSchoolState("SMALL-CITY");
+		assertTrue("The University's Location was set to SMALL-CITY", uni.getSchoolState().equals("SMALL-CITY"));
+	}
+	
+	@Test
+	public void setSchoolControlTest() {
+		uni.setSchoolControl("PUBLIC");
+		assertTrue("The University's Control was set to PUBLIC", uni.getSchoolControl().equals("PUBLIC"));
+	}
+	
+	@Test
+	public void setNumberStudentsTest() {
+		uni.setNumberStudents(20000);
+		assertTrue("The University's Number of Students was set to 20000", uni.getNumberStudents() == 20000);
+		assertFalse("The University's Number of Students was NOT set to 20000", uni.getNumberStudents() != 20000);
+	}
+	
+	@Test
+	public void setPercentFemaleTest() {
+		uni.setPercentFemale(80);
+		assertTrue("The University's Percent Female was set to 80", uni.getPercentFemale() == 80);
+		assertFalse("The University's Percent Female was NOT set to 80", uni.getPercentFemale() != 80);
+	}
+	
+	@Test
+	public void setVerbalSATTest() {
+		uni.setVerbalSAT(600);
+		assertTrue("The University's Verbal SAT was set to 600", uni.getVerbalSAT() == 600);
+		assertFalse("The University's Verbal SAT was NOT set to 600", uni.getVerbalSAT() != 600);
+	}
+	
+	@Test
+	public void setMathSATTest() {
+		uni.setMathSAT(600);
+		assertTrue("The University's Math SAT was set to 600", uni.getMathSAT() == 600);
+		assertFalse("The University's Math SAT was NOT set to 600", uni.getMathSAT() != 600);
+	}
+	
+	@Test
+	public void setSchoolExpensesTest() {
+		uni.setSchoolExpenses(40000);
+		assertTrue("The University's School Expenses was set to 40000", uni.getSchoolExpenses() == 40000);
+		assertFalse("The University's School Expenses was NOT set to 40000", uni.getSchoolExpenses() != 40000);
+	}
+	
+	@Test
+	public void setPercentFinancialAidTest() {
+		uni.setPercentFinancialAid(5);
+		assertTrue("The University's Percent Financial Aid was set to 5", uni.getPercentFinancialAid() == 5);
+		assertFalse("The University's Percent Financial Aid was NOT set to 5", uni.getPercentFinancialAid() != 5);
+	}
+	
+	@Test
+	public void setNumApplicantsTest() {
+		uni.setNumApplicants(500);
+		assertTrue("The University's Number of Applicants was set to 500", uni.getNumApplicants() == 500);
+		assertFalse("The University's Number of Applicants was NOT set to 500", uni.getNumApplicants() != 500);
 	}
 }
